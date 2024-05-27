ef6460dcfa696ce25023e9d294b971c364469e12
Bruno <b.juste@fp.safavalladolid.com>
2024-05-27 09:55:45 +0200

    dia 27_05_2024

--- Tema7/EjercicioT7_7.sql
+++ Tema7/EjercicioT7_7.sql
@@ -1,24 +1,19 @@
+set serveroutput on;
 DECLARE
-    profesorUno profesor;
-    alumnoUno alumno;
-BEGIN
-    profesorUno := profesor(
-        101, TNOMBRECOMPLETO('Dolores', 'SANCHEZ', 'GOMEZ'), TDOMICILIO('Plaza', 'Espa�a', 103, '1oA'), TCODPOSTAL(28003, 28), DATE '2021-09-01'
-    );
+    dom1 tdomicilio := new tdomicilio('Plaza', 'Espania', 103, '1oA');
+    cod1 tcodpostal := new tcodpostal(003, 28);
+    nombre tnombrecompleto := new tnombrecompleto('Dolores', 'SANCHEZ', 'GOMEZ');
+    profesorUno profesor := new profesor (101,nombre,dom1,cod1,sysdate,'1/9/2021');
 
-    alumnoUno := alumno(
-        201, TNOMBRECOMPLETO('Luis', 'MARIN', 'SOL'), TDOMICILIO('Calle', 'Laguna', 12, ''), TCODPOSTAL(28660, 28), DATE '2000-01-01', EVALUACIONES(4, 4, 5, 7) 
-    );
+    dom2 tdomicilio := new tdomicilio('golondrinas', 'pericas', 12, '1oA');
+    cod2 tcodpostal := new tcodpostal(660, 28);
+    nombre tnombrecompleto := new tnombrecompleto('Luis', 'MARIN', 'SOL');
+    alumnoUno alumno := new alumno (201,)
+    
+BEGIN
 
     DBMS_OUTPUT.PUT_LINE('Profesor: ');
     DBMS_OUTPUT.PUT_LINE('Nombre: ' || profesorUno.NOMBRE_COMPLETO.NOMBRE || ' ' || profesorUno.NOMBRE_COMPLETO.PRAPELLIDO || ' ' || profesorUno.NOMBRE_COMPLETO.SGAPELLIDO);
-    DBMS_OUTPUT.PUT_LINE('Fecha de Incorporaci�n: ' || TO_CHAR(profesorUno.F_INCORPORACION, 'DD/MM/YYYY'));
-
-    DBMS_OUTPUT.PUT_LINE('Alumno: ');
-    DBMS_OUTPUT.PUT_LINE('Nombre: ' || alumnoUno.NOMBRE_COMPLETO.NOMBRE || ' ' || alumno_obj.NOMBRE_COMPLETO.PRAPELLIDO || ' ' || alumnoUno.NOMBRE_COMPLETO.SGAPELLIDO);
-    DBMS_OUTPUT.PUT_LINE('Calificaciones:');
-    DBMS_OUTPUT.PUT_LINE('Evaluaci�n 1: ' || alumnoUno.CALIFICACIONES.EVALUACIONUNO);
-    DBMS_OUTPUT.PUT_LINE('Evaluaci�n 2: ' || alumnoUno.CALIFICACIONES.EVALUACIONDOS);
-    DBMS_OUTPUT.PUT_LINE('Evaluaci�n 3: ' || alumnoUno.CALIFICACIONES.EVALUACIONTRES);
-    DBMS_OUTPUT.PUT_LINE('Evaluaci�n 4: ' || alumnoUno.CALIFICACIONES.EVALUACIONCUATRO);
+    DBMS_OUTPUT.PUT_LINE('Fecha de Incorporacion: ' || TO_CHAR(profesorUno.F_INCORPORACION, 'DD/MM/YYYY'));
+    
 END;
